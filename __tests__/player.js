34b08c084287d6dbf5b3e9428671bb132b9f5ee0
jinimcoroneo <jinim1983@gmail.com>
2019-10-11 17:27:26 -0600

    add tests for player receiving cards

--- __tests__/player.js
+++ __tests__/player.js
@@ -1,9 +1,19 @@
 const Player = require("../src/player")
+const Card = require("../src/card")
+
 
 describe("Player", () => {
     it("can be created with a name", () => {
         const player = new Player("Jini")
         expect(player.name).toEqual("Jini")
+    })
+
+    it("can collect cards", () => {
+        const player = new Player("Jini")
+        const card = new Card('S', 'A')
+        player.collect([card])
+        expect(player.cards.length).toEqual(1)
+        expect(player.cards[0]).toEqual(card)
     })
 
 })
